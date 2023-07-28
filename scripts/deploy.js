d45542235effa232b4c2810595a5495536f3fd25
Rahul-Prasad-07 <prasadrahulprn3@gmail.com>
2023-07-28 14:48:10 +0530

    added deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,22 +7,32 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  //deploy the token contract
+  const tokenContract = await hre.ethers.deployContract("Token");
+  await tokenContract.waitForDeployment();
+  console.log("Token deployed to:", tokenContract.target);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  //deploy the Exchange contract
+  const exchangeContract = await hre.ethers.deployContract("Exchange", [
+    tokenContract.target,
+  ]);
+  await exchangeContract.waitForDeployment();
+  console.log("Exchange deployed to:", exchangeContract.target);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
+  // wait for 30 seconds to let etherescan update
+  await sleep(30 * 1000);
+
+  // verify the contract on etherscan
+  await hre.run("verify:verify", {
+    address: tokenContract.target,
+    constructorArguments: [],
+    contract: "contracts/Token.sol:Token",
   });
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  await hre.run("verify:verify", {
+    address: exchangeContract.target,
+    constructorArguments: [tokenContract.target],
+  });
 }
 
 // We recommend this pattern to be able to use async/await everywhere
